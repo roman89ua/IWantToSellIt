a3bcfe9e575b76d11b4a15c323369ff810a271ae
roman89ua <roman89ua@gmail.com>
2021-01-28 16:52:52 +0200

    reinstall IDE and reset up setings for IDE

--- App.js
+++ App.js
@@ -1,32 +1,6 @@
 import React /*, { useState }*/ from "react";
 import LoginScreen from "./app/screens/LoginScreen";
 
-// import Screen from "./app/components/Screen";
-//
-// import AppPicker from "./app/components/AppPicker";
-// import AppTextInput from "./app/components/AppTextInput";
-
-// const categories = [
-//   { label: "Clothing", value: 1 },
-//   { label: "Furniture", value: 2 },
-//   { label: "Technician", value: 3 },
-// ];
-
 export default function App() {
-  // const [category, setCategory] = useState();
-  return (
-    // <Screen>
-    //   <AppPicker
-    //     selectedItem={category}
-    //     onSelectItem={(item) => setCategory(item)}
-    //     items={categories}
-    //     iconName="apps"
-    //     placeholder="Category"
-    //   />
-    //   <AppTextInput iconName="email" placeholder="Email">
-    //     {category}
-    //   </AppTextInput>
-    // </Screen>
-    <LoginScreen />
-  );
+  return <LoginScreen />;
 }
